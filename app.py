9fbdde8513a616e294bd3da54b2dd17b3e94af32
Juan Jose Velez <83320302+jjvelezo@users.noreply.github.com>
2023-11-13 17:12:52 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, jsonify
+import random
+from pokeneas import pokeneas
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/pokenea_json')
+def get_random_pokenea():
+    random_pokenea = random.choice(pokeneas)
+    return jsonify({
+        "id": random_pokenea["id"],
+        "nombre": random_pokenea["nombre"],
+        "altura": random_pokenea["altura"],
+        "habilidad": random_pokenea["habilidad"],
+        "id_contenedor": id(app)
+    })
+
+@app.route('/pokenea')
+def get_random_pokenea_info():
+    random_pokenea = random.choice(pokeneas)
+    return render_template('home.html', pokenea=random_pokenea, id_contenedor=id(app))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
+
+
---
+++ pokeneas.py
@@ -0,0 +1,61 @@
+pokeneas = [
+    {"id": 1, 
+     "nombre": "Yorbyn", 
+     "altura": 1.7, 
+     "habilidad": "Sabe desarmar una moto en 10 minutos", 
+     "imagen": "https://storage.googleapis.com/bucketjjv/pokenea1.png", 
+     "frase_filosofica": "Estoy ofendido, Se me va bajando de celuco socio"
+     },
+
+    {"id": 2, 
+     "nombre": "Bairon Esneyder", 
+     "altura": 1.6, 
+     "habilidad": "Sabe picar la DT", 
+     "imagen": "https://storage.googleapis.com/bucketjjv/pokenea2.png", 
+     "frase_filosofica": "Solo sople, guaracha y gatas, si sabe"
+     },
+
+    {"id": 3, 
+     "nombre": "Jose Jainiber", 
+     "altura": 1.73, 
+     "habilidad": "Te vende una loca preñada", 
+     "imagen": "https://storage.googleapis.com/bucketjjv/pokenea3.png", 
+     "frase_filosofica": "Pilas Muñeco"
+     },
+
+    {"id": 4, 
+     "nombre": "Care Palo", 
+     "altura": 1.62, 
+     "habilidad": "Te arma un cuchillo con un cepillo de dientes", 
+     "imagen": "https://storage.googleapis.com/bucketjjv/pokenea4.png", 
+     "frase_filosofica": "Fino mi parcero"
+     },
+
+    {"id": 5, 
+     "nombre": "Rostro de Chucha", 
+     "altura": 1.71, 
+     "habilidad": "Sabe tirar mucha labia", 
+     "imagen": "https://storage.googleapis.com/bucketjjv/pokenea5.png", 
+     "frase_filosofica": "Todo bien mi niño"
+     },
+
+     {"id": 6, 
+     "nombre": "Pan de lola", 
+     "altura": 1.79, 
+     "habilidad": "Gota gota", 
+     "imagen": "https://storage.googleapis.com/bucketjjv/pokenea6.png", 
+     "frase_filosofica": "Pase pues las lukas pa comprar el chorro"
+     },
+
+     {"id": 7, 
+     "nombre": "La Rata", 
+     "altura": 1.74, 
+     "habilidad": "Se escoden facilmente de la policia", 
+     "imagen": "https://storage.googleapis.com/bucketjjv/pokenea7.png", 
+     "frase_filosofica": "En la juega con los tombos, serveras locas szs"
+     },
+
+
+
+  
+]
